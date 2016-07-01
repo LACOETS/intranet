31096ab6f296632babcc4e50ec0dbcd758276de3
LACOETS <garcia_edward@lacoe.edu>
2016-07-02 03:36:14 +0530

    Add files via upload

---
+++ Control_List_bck.js
@@ -0,0 +1,73 @@
+function ULS9sP(){var o=new Object;o.ULSTeamName="Search Server";o.ULSFileName="Control_List_bck.js";return o;}
+function DisplayTemplate_3fbeb40cbae94e998c12fabd0092a478(ctx) {ULS9sP:;
+  var ms_outHtml=[];
+  var cachePreviousTemplateData = ctx['DisplayTemplateData'];
+  ctx['DisplayTemplateData'] = new Object();
+  DisplayTemplate_3fbeb40cbae94e998c12fabd0092a478.DisplayTemplateData = ctx['DisplayTemplateData'];
+
+  ctx['DisplayTemplateData']['TemplateUrl']='~sitecollection\u002f_catalogs\u002fmasterpage\u002fDisplay Templates\u002fContent Web Parts\u002fControl_List_bck.js';
+  ctx['DisplayTemplateData']['TemplateType']='Control';
+  ctx['DisplayTemplateData']['TargetControlType']=['Content Web Parts'];
+  this.DisplayTemplateData = ctx['DisplayTemplateData'];
+
+ms_outHtml.push('',''
+,''
+); 
+if (!$isNull(ctx.ClientControl) &&
+    !$isNull(ctx.ClientControl.shouldRenderControl) &&
+    !ctx.ClientControl.shouldRenderControl())
+{
+    return "";
+}
+ctx.ListDataJSONGroupsKey = "ResultTables";
+var $noResults = Srch.ContentBySearch.getControlTemplateEncodedNoResultsMessage(ctx.ClientControl);
+
+var noResultsClassName = "ms-srch-result-noResults";
+
+var ListRenderRenderWrapper = function(itemRenderResult, inCtx, tpl)
+{ULS9sP:;
+    var iStr = [];
+    iStr.push('<li>');
+    iStr.push(itemRenderResult);
+    iStr.push('</li>');
+    return iStr.join('');
+}
+ctx['ItemRenderWrapper'] = ListRenderRenderWrapper;
+ms_outHtml.push(''
+,'    <ul class="cbs-List">'
+,''
+,'            ', ctx.RenderGroups(ctx) ,''
+,'        </ul>'
+);
+if (ctx.ClientControl.get_shouldShowNoResultMessage())
+{
+ms_outHtml.push(''
+,'        <div class="', noResultsClassName ,'">', $noResults ,'</div>'
+);
+}
+ms_outHtml.push(''
+,''
+,'    '
+);
+
+  ctx['DisplayTemplateData'] = cachePreviousTemplateData;
+  return ms_outHtml.join('');
+}
+function RegisterTemplate_3fbeb40cbae94e998c12fabd0092a478() {ULS9sP:;
+
+if ("undefined" != typeof (Srch) &&"undefined" != typeof (Srch.U) &&typeof(Srch.U.registerRenderTemplateByName) == "function") {
+  Srch.U.registerRenderTemplateByName("Control_List_bck", DisplayTemplate_3fbeb40cbae94e998c12fabd0092a478);
+}
+
+if ("undefined" != typeof (Srch) &&"undefined" != typeof (Srch.U) &&typeof(Srch.U.registerRenderTemplateByName) == "function") {
+  //Srch.U.registerRenderTemplateByName("~sitecollection\u002f_catalogs\u002fmasterpage\u002fDisplay Templates\u002fContent Web Parts\u002fControl_List_bck.js", DisplayTemplate_3fbeb40cbae94e998c12fabd0092a478);
+  Srch.U.registerRenderTemplateByName("https://LACOETS.github.io\intranet\Control_List_bck.js", DisplayTemplate_3fbeb40cbae94e998c12fabd0092a478);
+}
+
+        $includeLanguageScript("~sitecollection\u002f_catalogs\u002fmasterpage\u002fDisplay Templates\u002fContent Web Parts\u002fControl_List_bck.js", "~sitecollection/_catalogs/masterpage/Display Templates/Language Files/{Locale}/CustomStrings.js");
+
+}
+RegisterTemplate_3fbeb40cbae94e998c12fabd0092a478();
+if (typeof(RegisterModuleInit) == "function" && typeof(Srch.U.replaceUrlTokens) == "function") {
+  RegisterModuleInit(Srch.U.replaceUrlTokens("https://LACOETS.github.io\intranet\Control_List_bck.js"), RegisterTemplate_3fbeb40cbae94e998c12fabd0092a478);
+}
