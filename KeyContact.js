da811a1dd64625880e2fd70f6ec59fa4a43df086
LACOETS <garcia_edward@lacoe.edu>
2016-07-07 02:22:11 -0700

    Add files via upload

--- KeyContact.js
+++ KeyContact.js
@@ -35,6 +35,7 @@ $(function($, undefined) {
 		{
 		var userEntry = {};
 		userEntry.Name = dataRec.Email; // substring email to get user name
+		alert(dataRec.Email);
 		userEntry.Title = dataRec.Job_x0020_Title;
 		userEntry.Email = dataRec.Email;
 		userEntry.Category=dataRec.KeyContacts_x0020_Category;
