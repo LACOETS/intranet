4a27c9e2586adb875ec4e9b14b1ea19c0ecb917f
LACOETS <garcia_edward@lacoe.edu>
2016-07-08 14:51:14 -0700

    Add files via upload

--- control_list_bck.js
+++ control_list_bck.js
@@ -1,4 +1,4 @@
-function ULS9sP(){var o=new Object;o.ULSTeamName="Search Server";o.ULSFileName="Control_List_bck.js";return o;}
+
 function DisplayTemplate_3fbeb40cbae94e998c12fabd0092a478(ctx) {ULS9sP:;
   var ms_outHtml=[];
   var cachePreviousTemplateData = ctx['DisplayTemplateData'];
@@ -53,23 +53,4 @@ ms_outHtml.push(''
   ctx['DisplayTemplateData'] = cachePreviousTemplateData;
   return ms_outHtml.join('');
 }
-function RegisterTemplate_3fbeb40cbae94e998c12fabd0092a478() {ULS9sP:;
 
-if ("undefined" != typeof (Srch) &&"undefined" != typeof (Srch.U) &&typeof(Srch.U.registerRenderTemplateByName) == "function") {
-  Srch.U.registerRenderTemplateByName("Control_List_bck", DisplayTemplate_3fbeb40cbae94e998c12fabd0092a478);
-}
-
-if ("undefined" != typeof (Srch) &&"undefined" != typeof (Srch.U) &&typeof(Srch.U.registerRenderTemplateByName) == "function") {
-  //Srch.U.registerRenderTemplateByName("~sitecollection\u002f_catalogs\u002fmasterpage\u002fDisplay Templates\u002fContent Web Parts\u002fControl_List_bck.js", DisplayTemplate_3fbeb40cbae94e998c12fabd0092a478);
-  Srch.U.registerRenderTemplateByName("https://LACOETS.github.io\u002fintranet\u002fControl_List_bck.js", DisplayTemplate_3fbeb40cbae94e998c12fabd0092a478);
-  
-}
-
-        $includeLanguageScript("~sitecollection\u002f_catalogs\u002fmasterpage\u002fDisplay Templates\u002fContent Web Parts\u002fControl_List_bck.js", "~sitecollection/_catalogs/masterpage/Display Templates/Language Files/{Locale}/CustomStrings.js");
-
-}
-RegisterTemplate_3fbeb40cbae94e998c12fabd0092a478();
-if (typeof(RegisterModuleInit) == "function" && typeof(Srch.U.replaceUrlTokens) == "function") {
-  RegisterModuleInit(Srch.U.replaceUrlTokens("https://LACOETS.github.io\u002fintranet\u002fControl_List_bck.js"), RegisterTemplate_3fbeb40cbae94e998c12fabd0092a478);
-  
-}
