0ed403182d5802bb4e8d43234848e96474850e8a
LACOETS <garcia_edward@lacoe.edu>
2016-07-19 11:45:12 +0530

    Update Employee-Spotlight-RESTAPI.js

--- Employee-Spotlight-RESTAPI.js
+++ Employee-Spotlight-RESTAPI.js
@@ -12,7 +12,7 @@ $(function($, undefined) {
       var context = SP.ClientContext.get_current();
       var todayDate = new Date().toISOString().split('T')[0];
 	  todayDate = todayDate + 'T00%3a00%3a00';	  
-	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('Employee Spotlight')/items?$expand=Employee_x0020_Name/ID&$select=Employee_x0020_Name/FirstName,Employee_x0020_Name/LastName,Title,Department,Description,Start_x0020_Date,End_x0020_Date,Picture_x0020_URL&$orderby=End_x0020_Date asc&$filter=Start_x0020_Date le datetime'" + todayDate + "' and End_x0020_Date ge datetime'" + todayDate + "'"; 
+	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('Employee Spotlight')/items?$expand=Employee_x0020_Name/ID&$select=Employee_x0020_Name/FirstName,Employee_x0020_Name/LastName,Employee_x0020_Name/EMail,Title,Department,Description,Start_x0020_Date,End_x0020_Date,Picture_x0020_URL&$orderby=End_x0020_Date asc&$filter=Start_x0020_Date le datetime'" + todayDate + "' and End_x0020_Date ge datetime'" + todayDate + "'"; 
       $.ajax({
         url: queryUrl,
         method: "GET",
@@ -30,12 +30,17 @@ $(function($, undefined) {
     $.each(results, function(index, dataRec) {
 		//alert(dataRec);
 		var userEntry = {};
-		if(dataRec.Employee_x0020_Name.FirstName == null)
-			{dataRec.Employee_x0020_Name.FirstName = '';}
-		if(dataRec.Employee_x0020_Name.LastName == null)
-			{dataRec.Employee_x0020_Name.LastName = '';}	
-		userEntry.Name = dataRec.Employee_x0020_Name.FirstName + " " + dataRec.Employee_x0020_Name.LastName;
-		//alert(userEntry.Name);
+		if(dataRec.Employee_x0020_Name.FirstName == null || dataRec.Employee_x0020_Name.LastName == null)
+			{
+				userEntry.Name = dataRec.Employee_x0020_Name.EMail.split('@')[0];
+			}
+		else
+			{			    
+			    if(dataRec.Employee_x0020_Name.LastName == null)
+					{dataRec.Employee_x0020_Name.LastName = '';}
+				userEntry.Name = dataRec.Employee_x0020_Name.FirstName + " " + dataRec.Employee_x0020_Name.LastName;
+			}
+
 		userEntry.Title = dataRec.Title;
 		//alert(userEntry.Title);
 		userEntry.Department = dataRec.Department;
