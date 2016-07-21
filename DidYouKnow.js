54077ae0d27cf6ceda8cc9f3417499ca7d78e253
LACOETS <garcia_edward@lacoe.edu>
2016-07-21 16:03:13 +0530

    Update DidYouKnow.js

--- DidYouKnow.js
+++ DidYouKnow.js
@@ -3,12 +3,13 @@
 <script>
 
 $(function($, undefined) {
-alert('123 Vipul');
+
   SP.SOD.executeFunc("sp.js", "SP.ClientContext", function() {
     SP.SOD.executeFunc("sp.runtime.js", "SP.ClientContext", function() {
       var todayDate = new Date().toISOString().split('T')[0];
 	  todayDate = todayDate + 'T00%3a00%3a00';	  
 	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('Did You Know')/items?$select=Title,Start_x0020_Date,End_x0020_Date,Picture_x0020_URL&$orderby=End_x0020_Date asc&$filter=Start_x0020_Date le datetime'" + todayDate + "' and End_x0020_Date ge datetime'" + todayDate + "'"; 
+      alert(queryUrl);
       $.ajax({
         url: queryUrl,
         method: "GET",
@@ -26,7 +27,7 @@ alert('123 Vipul');
     $.each(results, function(index, dataRec) {
 		userEntry.Title = dataRec.Title;		
 		userEntry.ImageURL=dataRec.Picture_x0020_URL.Url;
-		
+		alert(userEntry.ImageURL);
 		$('.ulDidYouKnow').append('<li><div class="img-box-thumb-holder"><div class="spotlight-img"><img src="'+userEntry.ImageURL+'" id="SpotlightImage" alt="Image1"/></div><p>'+userEntry.Title+'</p></div></div></li>');
     });
   }
