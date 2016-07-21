51dd17d1b8347fa25fb1277525a8197301a4891d
LACOETS <garcia_edward@lacoe.edu>
2016-07-21 15:33:47 +0530

    Add files via upload

---
+++ DidYouKnow.js
@@ -0,0 +1,38 @@
+<ul class="ulDidYouKnow"></ul>
+
+<script>
+
+$(function($, undefined) {
+
+  SP.SOD.executeFunc("sp.js", "SP.ClientContext", function() {
+    SP.SOD.executeFunc("sp.runtime.js", "SP.ClientContext", function() {
+      var context = SP.ClientContext.get_current();
+      var todayDate = new Date().toISOString().split('T')[0];
+	  todayDate = todayDate + 'T00%3a00%3a00';	  
+	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitleDid You Know')/items?Title,Start_x0020_Date,End_x0020_Date,Picture_x0020_URL&$orderby=End_x0020_Date asc&$filter=Start_x0020_Date le datetime'" + todayDate + "' and End_x0020_Date ge datetime'" + todayDate + "'"; 
+      $.ajax({
+        url: queryUrl,
+        method: "GET",
+        headers: {
+          "Accept": "application/json; odata=verbose"
+        },
+        success: onQuerySuccess,
+        error: onQueryError
+      });
+    });
+  });
+
+  function onQuerySuccess(data) {
+    var results = data.d.results;	
+    $.each(results, function(index, dataRec) {
+		userEntry.Title = dataRec.Title;		
+		userEntry.ImageURL=dataRec.Picture_x0020_URL.Url;
+		
+		$('.ulDidYouKnow').append('<li><div class="img-box-thumb-holder"><div class="spotlight-img"><img src="'+userEntry.ImageURL+'" id="SpotlightImage" alt="Image1"/></div><p>'+userEntry.Title+'</p></div></div></li>');
+    });
+  }
+  function onQueryError(error) {
+    $(".spotlight-slider").append(error.statusText)
+  }
+});
+</script>
