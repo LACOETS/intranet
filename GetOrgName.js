ed5fa4b3d797fa817ec7514baf4939fc1a87ad7d
LACOETS <garcia_edward@lacoe.edu>
2016-07-26 09:56:32 +0530

    Update GetOrgName.js

--- GetOrgName.js
+++ GetOrgName.js
@@ -1,6 +1,6 @@
 <script type="text/javascript">
 $(document).ready(function() {
     $('input[title="Org Required Field"]').val(_spPageContextInfo.webTitle);
-    $('input[title="Org Required Field"]').prop('disabled', true);
+    $('input[title="Org Required Field"]').attr('readonly', true);
 });
 </script>
