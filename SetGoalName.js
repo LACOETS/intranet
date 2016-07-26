843fc5a901c2d965aa7031bc6e3b0fc54ffad316
LACOETS <garcia_edward@lacoe.edu>
2016-07-26 12:10:04 +0530

    Add files via upload

---
+++ SetGoalName.js
@@ -0,0 +1,53 @@
+﻿<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.SPServices/2014.02/jquery.SPServices-2014.02.min.js"></script>
+
+<script type="text/javascript">
+var goalID;
+$(document).ready(function(){
+	//if(window.location.href.indexOf('NewForm.aspx') > -1){
+		goalID = decodeURIComponent(window.location.href.split('?')[1].split('=')[1]);
+		goalID = goalID.split('?')[1].split('=')[1].split('&')[0];
+		//alert('goalID:=' + goalID);
+		var goalName = GetGoalName(goalID);
+	//}//End of If
+});//End of Doc ready
+
+
+function GetGoalName(goalID){
+SP.SOD.executeFunc("sp.js", "SP.ClientContext", function() {
+    SP.SOD.executeFunc("sp.runtime.js", "SP.ClientContext", function() {      	  
+	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('Goals')/items?$select=Id,OrgGoals_x0020_GoalName,Title&$filter=Id eq '" + goalID + "'"; 
+      //alert(queryUrl);
+	  $.ajax({
+        url: queryUrl,
+        method: "GET",
+        headers: {
+          "Accept": "application/json; odata=verbose"
+        },
+        success: onQuerySuccess,
+        error: onQueryError
+      });
+    });
+  });
+}//End of GetGoalName
+
+function onQuerySuccess(data) {
+$('select[title="Goal Name Required Field"]').empty();
+var userEntry = [];
+var results = data.d.results;	
+    $.each(results, function(index, dataRec) {    
+    userEntry.push({
+				id:dataRec.Id,
+				name:dataRec.OrgGoals_x0020_GoalName		
+		});//End of push    
+ });//End of each
+ for(var j=0;j<userEntry.length;j++){
+			$('select[title="Goal Name Required Field"]').append($('<option>', { value: userEntry[j].id,text : userEntry[j].name}));	
+  	}//End of for
+  	//$('select[title="Goal Name Required Field"]').prop("disabled", true);
+}//End of onQuerySuccess
+
+function onQueryError(error) {
+    alert(error.statusText);
+  }//End of onQueryError
+</script>
