e3666b49510771421800c44dbe150b2ad544a098
LACOETS <garcia_edward@lacoe.edu>
2016-07-26 09:54:39 +0530

    Add files via upload

---
+++ GetOrgName.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+$(document).ready(function() {
+    $('input[title="Org Required Field"]').val(_spPageContextInfo.webTitle);
+    $('input[title="Org Required Field"]').prop('disabled', true);
+});
+</script>
