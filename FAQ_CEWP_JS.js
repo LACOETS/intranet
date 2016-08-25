47ba2254d0481b7f3edbdc13923d1873e45d8f91
LACOETS <garcia_edward@lacoe.edu>
2016-08-25 16:11:39 +0530

    Add files via upload

---
+++ FAQ_CEWP_JS.js
@@ -0,0 +1,51 @@
+<script type="text/javascript">
+$(document).ready(function ()
+{
+$(function($, undefined) 
+{
+  SP.SOD.executeFunc("sp.js", "SP.ClientContext", function() {
+    SP.SOD.executeFunc("sp.runtime.js", "SP.ClientContext", function() {
+      var context = SP.ClientContext.get_current(); 
+	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('FAQDetails')/items?$select=Title,Description"; 
+      $.ajax({
+        url: queryUrl,
+        method: "GET",
+        headers: {
+          "Accept": "application/json; odata=verbose"
+        },
+        success: onQuerySuccess,
+        error: onQueryError
+      });
+    });
+  });
+
+  function onQuerySuccess(data) {
+    var results = data.d.results;	
+    $.each(results, function(index, dataRec) {
+		var userEntry = {};
+		userEntry.Title = dataRec.Title;
+		userEntry.Description=dataRec.Description;
+		   $('#FAQAccordion').append("<dl>" + 
+                                	      "<dt>" + userEntry.Title  + "</dt>" + 
+                                	      "<dd>" + userEntry.Description  + "</dd>" + 
+                                       "</dl>");
+    });
+  }
+  function onQueryError(error) {
+    $("#keyContactPrimary").append(error.statusText)
+  }
+});
+});  // document ready
+</script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.faqs-pagination').pajinate({
+			num_page_links_to_display :false,
+			items_per_page : 5	
+		});
+	}); 
+</script> 
+
+ 
+ 
+ 
