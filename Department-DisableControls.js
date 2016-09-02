3d44257fa71da19312a97fd01ddbdc32618194e7
LACOETS <garcia_edward@lacoe.edu>
2016-09-02 18:43:21 +0530

    Update Department-DisableControls.js

--- Department-DisableControls.js
+++ Department-DisableControls.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 	$("input[title='Name Required Field']").prop("disabled", true);
 	$("input[title='Title']").prop("disabled", true);
 	$("select[title='Document Type']").prop("disabled", true);
+	$("select[title='Document Type Required Field']").prop("disabled", true);
 	$("div[id='DataClassificationRSP_$containereditableRegion']").prop('contenteditable',false);
 	$("[title='Browse for a valid choice']").hide();
 	$("textarea[title='Lib Description']").attr('disabled','disabled');
