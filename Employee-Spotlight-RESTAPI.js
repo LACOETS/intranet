42478c705ebfd3ccc263eec793088b3d2cdfad3a
LACOETS <garcia_edward@lacoe.edu>
2016-09-02 09:17:35 +0530

    Update Employee-Spotlight-RESTAPI.js

--- Employee-Spotlight-RESTAPI.js
+++ Employee-Spotlight-RESTAPI.js
@@ -9,7 +9,7 @@ $(function($, undefined) {
       var context = SP.ClientContext.get_current();
       var todayDate = new Date().toISOString().split('T')[0];
 	  todayDate = todayDate + 'T00%3a00%3a00';	  
-	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('Employee Spotlight')/items?$expand=Employee_x0020_Name/ID&$select=Employee_x0020_Name/FirstName,Employee_x0020_Name/LastName,Employee_x0020_Name/EMail,Title,Department,Description,Start_x0020_Date,End_x0020_Date,Picture_x0020_URL&$orderby=End_x0020_Date asc&$filter=Start_x0020_Date le datetime'" + todayDate + "' and End_x0020_Date ge datetime'" + todayDate + "'"; 
+	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('Employee Spotlight')/items?$expand=Employee_x0020_Name/ID&$select=Employee_x0020_Name/FirstName,Employee_x0020_Name/LastName,Employee_x0020_Name/EMail,Title,Department,Description,Start_x0020_Date,End_x0020_Date,Picture_x0020_URL,Email&$orderby=End_x0020_Date asc&$filter=Start_x0020_Date le datetime'" + todayDate + "' and End_x0020_Date ge datetime'" + todayDate + "'"; 
       $.ajax({
         url: queryUrl,
         method: "GET",
@@ -37,13 +37,27 @@ $(function($, undefined) {
 				userEntry.Name = dataRec.Employee_x0020_Name.FirstName + " " + dataRec.Employee_x0020_Name.LastName;
 			}
 
-		userEntry.Title = dataRec.Title;
+		if(dataRec.Title != "" || dataRec.Title != " " || dataRec.Title != null || dataRec.Title != undefined){
+			userEntry.Title = dataRec.Title;
+		}
+		else
+		{
+			userEntry.Title = "--";
+		}
 		//alert(userEntry.Title);
-		userEntry.Department = dataRec.Department;
+		userEntry.Email = dataRec.Email;
+		if(dataRec.Department != "" || dataRec.Department != " " || dataRec.Department != null || dataRec.Department != undefined){
+			userEntry.Department = dataRec.Department;
+		}
+		else
+		{
+			userEntry.Department = "--";
+		}
 		userEntry.Description=dataRec.Description;
-		userEntry.ImageURL=dataRec.Picture_x0020_URL.Url;
+		//userEntry.ImageURL=dataRec.Picture_x0020_URL.Url;
+		var userImage= "https://outlook.office365.com/owa/service.svc/s/GetPersonaPhoto?email='"+userEntry.Email+"'&UA=0&size=HR240x240&sc=1468354588706";
 		
-		$('.spotlight-slider').append('<li><div class="img-box-thumb-holder"><div class="spotlight-img"><img src="https://lacoets.github.io/intranet/gray-img.png" data-original="'+userEntry.ImageURL+'" id="SpotlightImage" class="lazy" alt="Image1"/></div><div class="spotlight-infotxt"><h4>'+userEntry.Name+'</h4><div class="spotlight"><p class="departspot">'+userEntry.Title+ '<span class="sep-spotlight">|</span>' + userEntry.Department +'</p></div><p>'+userEntry.Description+'</p></div></div></li>');
+		$('.spotlight-slider').append('<li><div class="img-box-thumb-holder"><div class="spotlight-img"><img src="https://lacoets.github.io/intranet/gray-img.png" data-original="'+userImage+'" id="SpotlightImage" class="lazy" alt="Image1"/></div><div class="spotlight-infotxt"><h4>'+userEntry.Name+'</h4><div class="spotlight"><p class="departspot">'+userEntry.Title+ '<span class="sep-spotlight">|</span>' + userEntry.Department +'</p></div><p>'+userEntry.Description+'</p></div></div></li>');
     });
     sliderdiyou=$(".spotlight-slider").bxSlider({
 	        controls: false,
