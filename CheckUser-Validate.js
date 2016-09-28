45f1642a5d7e92b341bc39fee813303790a8bfa0
LACOETS <garcia_edward@lacoe.edu>
2016-09-28 14:26:38 -0700

    Add files via upload

--- CheckUser-Validate.js
+++ CheckUser-Validate.js
@@ -16,7 +16,7 @@ function PreSaveAction() {
 
 function checkDuplicateEntry()
 { 	var _PeoplePicker = $("div[title='Employee Name']");
-console.log(_PeoplePicker);
+	alert(_PeoplePicker);
     var _PeoplePickerTopId = _PeoplePicker.attr('id');
 	alert(_PeoplePickerTopId);
 	var result = false;
