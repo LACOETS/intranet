293a1492d4fef19771a5a111638a4b68578fff0e
LACOETS <garcia_edward@lacoe.edu>
2016-09-28 17:52:33 +0530

    Update custom-plugins.js

--- custom-plugins.js
+++ custom-plugins.js
@@ -79,8 +79,8 @@ $(window).load(function() {
 	annoucslider=$('#example5').sliderPro({
 			loop: true,
 			
-			thumbnailWidth: 516,
-			thumbnailHeight: 48,
+			/*thumbnailWidth: 516,
+			thumbnailHeight: 48,*/
 			autoHeight: true,
 			arrows: true,
 			buttons: false,
