0ab3c852e5e203997da9c5e12f2ef1fbdbdae111
LACOETS <garcia_edward@lacoe.edu>
2016-10-13 22:40:44 +0530

    Update SetGoalName.js

--- SetGoalName.js
+++ SetGoalName.js
@@ -33,11 +33,11 @@ $(document).ready(function(){
 });//End of Doc ready
 
 function GetFirstItemID(){
-	//alert('In GetFirstItemID');
+	alert('In GetFirstItemID');
 SP.SOD.executeFunc("sp.js", "SP.ClientContext", function() {
     SP.SOD.executeFunc("sp.runtime.js", "SP.ClientContext", function() {      	  
 	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('Goals')/items?$select=Id&$top 1"; 
-      //alert(queryUrl);
+      alert(queryUrl);
 	  $.ajax({
         url: queryUrl,
         method: "GET",
@@ -50,7 +50,7 @@ SP.SOD.executeFunc("sp.js", "SP.ClientContext", function() {
 				var userEntry = {};
 				userEntry.Id = dataRec.Id;
 				goalID = userEntry.Id;
-				//alert(goalID);
+				alert(goalID);
 			});
 	},
         error: onQueryError23
