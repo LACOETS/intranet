445d939283cef5ba36d19599b7455c19db4b73ea
LACOETS <garcia_edward@lacoe.edu>
2016-11-02 00:19:31 +0530

    Add files via upload

--- Local_Doc_library_editform.js
+++ Local_Doc_library_editform.js
@@ -1,9 +1,7 @@
 <script>
  $( document ).ready(function() {
-	 alert('Item updated successfully');
-	 alert($("select[title='Belongs to Project Required Field']option:selected").text());
-	 debugger;
-	 if ($("select[title='Belongs to Project Required Field']option:selected").text() == "")
+	 
+	 if ($("select[title='Belongs to Project Required Field'] :selected").text() == "")
 	{
 		
 	   $("select[title='Belongs to Project Required Field']").prepend('<option value="" selected="selected">Select</option>');
