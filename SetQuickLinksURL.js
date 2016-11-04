14fe008a88a1714d4c98a63cd5e2583df3b10813
LACOETS <garcia_edward@lacoe.edu>
2016-11-04 15:41:03 +0530

    Delete SetQuickLinksURL.js

--- SetQuickLinksURL.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-$(document).ready(function (){
-var od = GetMySiteURL().replace("Blog/default.aspx","_layouts/15/onedrive.aspx");
-var obj = $("table[summary='My-Links'] tbody").find("tr > td").find("span a[title='My OneDrive']");
-$(obj).attr('href',od);
-});
-
-</script>
