b4d5cce9e1bf52208b100d66d9ac46437068842c
LACOETS <garcia_edward@lacoe.edu>
2016-11-04 10:33:48 +0530

    Delete GetSiteName.js

--- GetSiteName.js
+++
@@ -1,7 +0,0 @@
-
-<script type="text/javascript">
-$(document).ready(function() {
-    $('input[title="Site Name Required Field"]').val(_spPageContextInfo.webTitle);
-    $('input[title="Site Name Required Field"]').attr('readonly', true);
-});
-</script>
