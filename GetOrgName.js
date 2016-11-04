29dc95c89ec0ffb5ec81ca332363d4e1d1c69c4b
LACOETS <garcia_edward@lacoe.edu>
2016-11-04 10:31:43 +0530

    Delete GetOrgName.js

--- GetOrgName.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-$(document).ready(function() {
-    $('input[title="Org Required Field"]').val(_spPageContextInfo.webTitle);
-    $('input[title="Org Required Field"]').attr('readonly', true);
-});
-</script>
