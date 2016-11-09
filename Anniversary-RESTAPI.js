c3cd59a5572675662d33d007ec6c88d83459f8e2
LACOETS <garcia_edward@lacoe.edu>
2016-11-09 16:12:51 +0530

    Add files via upload

---
+++ Anniversary-RESTAPI.js
@@ -0,0 +1,76 @@
+﻿<div class="aniversary-pagination" id="aniversary-pagination">
+	<ul class="aniversarysul pagination-rowholder">           
+	</ul>
+	<div class="page_navigation"></div>
+	<div class="clear"></div>
+</div>
+
+<script>
+
+$(function($, undefined) {
+
+  SP.SOD.executeFunc("sp.js", "SP.ClientContext", function() {
+    SP.SOD.executeFunc("sp.runtime.js", "SP.ClientContext", function() {
+      var context = SP.ClientContext.get_current();
+      var todayDate = new Date().toISOString().split('T')[0];
+	  todayDate = todayDate + 'T00%3a00%3a00';	  
+	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('Anniversary')/items?$expand=Employee_x0020_Name/ID&$select=Employee_x0020_Name/FirstName,Employee_x0020_Name/LastName,Employee_x0020_Name/EMail,Title,Department,Description,Start_x0020_date,End_x0020_Date,Picture_x0020_URL,Email&$orderby=End_x0020_Date asc&$filter=Start_x0020_date le datetime'" + todayDate + "' and End_x0020_Date ge datetime'" + todayDate + "'"; 
+      $.ajax({
+        url: queryUrl,
+        method: "GET",
+        headers: {
+          "Accept": "application/json; odata=verbose"
+        },
+        success: onQuerySuccess,
+        error: onQueryError
+      });
+    });
+  });
+
+  function onQuerySuccess(data) {
+    var results = data.d.results;	
+    $.each(results, function(index, dataRec) {
+		var userEntry = {};
+		if(dataRec.Employee_x0020_Name.FirstName == null || dataRec.Employee_x0020_Name.LastName == null)
+			{
+				userEntry.Name = dataRec.Employee_x0020_Name.EMail.split('@')[0];
+			}
+		else
+			{			    
+			    if(dataRec.Employee_x0020_Name.LastName == null)
+					{dataRec.Employee_x0020_Name.LastName = '';}
+				userEntry.Name = dataRec.Employee_x0020_Name.FirstName + " " + dataRec.Employee_x0020_Name.LastName;
+			}
+		//alert(dataRec.Title);
+		if(dataRec.Title == null){
+			userEntry.Title = "--";
+		}
+		else
+		{
+			userEntry.Title = dataRec.Title;
+		}
+		
+		userEntry.Email = dataRec.Email;
+		//alert(dataRec.Department);
+		if(dataRec.Department == null){
+			userEntry.Department = "--";
+		}
+		else
+		{
+			userEntry.Department = dataRec.Department;
+		}
+		
+		
+		userEntry.Description=dataRec.Description;
+		//userEntry.ImageURL=dataRec.Picture_x0020_URL.Url;
+		
+		var userImage= "//outlook.office365.com/owa/service.svc/s/GetPersonaPhoto?email=" + userEntry.Email + "&UA=0&size=HR120x120&sc=1468354588706";        
+		
+		$('.aniversarysul').append('<li><div class="img-box-thumb-holder"><div class="box-rowcntr"><div class="box-img-thumbnail"><div class="img-box-thumb-holder"><img src="https://lacoets.github.io/intranet/gray-img.png" data-original="'+userImage+'" id="AnniversaryImage" class="lazy" alt="Image1"/></div></div><div class="news-info-cntr"><div class="news-title-like"><h4>'+userEntry.Name+'</h4></div><div class="news-activity-dtl"><p class="news-showdate">'+userEntry.Title+'<br>'+ userEntry.Department+'</p></div></div><div class="clear"></div></div></li>');
+    });
+  }
+  function onQueryError(error) {
+    $("#keyContactPrimary").append(error.statusText)
+  }
+});
+</script>
---
+++ DidYouKnow.js
@@ -0,0 +1,67 @@
+<ul class="ulDidYouKnow"></ul>
+
+<script>
+var sliderdiyou;
+$(function($, undefined) {
+  SP.SOD.executeFunc("sp.js", "SP.ClientContext", function() {
+    SP.SOD.executeFunc("sp.runtime.js", "SP.ClientContext", function() {
+      var todayDate = new Date().toISOString().split('T')[0];
+	  todayDate = todayDate + 'T00%3a00%3a00';	  
+	  var queryUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('Did You Know')/items?$select=Title,Start_x0020_Date,End_x0020_Date,Picture_x0020_URL,Landing_x0020_Site_x0020_Approva&$orderby=End_x0020_Date asc&$filter=Start_x0020_Date le datetime'" + todayDate + "' and End_x0020_Date ge datetime'" + todayDate + "' and Landing_x0020_Site_x0020_Approva eq 'Yes'"; 
+      
+      $.ajax({
+        url: queryUrl,
+        method: "GET",
+        headers: {
+          "Accept": "application/json; odata=verbose"
+        },
+        success: onQuerySuccess,
+        error: onQueryError
+      });
+    });
+  });
+
+  function onQuerySuccess(data) {
+    var results = data.d.results;	
+    $.each(results, function(index, dataRec) {
+		var userEntry = {};
+		userEntry.Title = dataRec.Title;		
+		userEntry.ImageURL=dataRec.Picture_x0020_URL.Url;
+		
+		if(userEntry.ImageURL.indexOf('.jpeg') > -1 || userEntry.ImageURL.indexOf('.jpg') > -1 || userEntry.ImageURL.indexOf('.gif') > -1 || userEntry.ImageURL.indexOf('.png') > -1)
+		{
+			userEntry.ImageURL = dataRec.Picture_x0020_URL.Url;
+		}
+		else
+		{
+			userEntry.ImageURL = "https://lacoets.github.io/intranet/DUK.png";
+		}
+		$('.ulDidYouKnow').append('<li><div class="didyouknwbx"><div class="didyouknwbx-img"><img src="'+userEntry.ImageURL+'" id="SpotlightImage" alt="Image1"/></div><div class="didyouknw-txt"><p>'+userEntry.Title+'</p></div><div class="clear"></div></div></div></li>');
+    });
+		
+	/*Showing slider condition*/
+		sliderdiyou=$('.ulDidYouKnow').bxSlider({
+			controls:false,
+			pause:7000,
+			speed:1000,
+			infiniteLoop: false,
+			auto: ($(".ulDidYouKnow li").length > 1) ? true: false,
+		    	pager: ($(".ulDidYouKnow li").length > 1) ? true: false
+		});
+	/*Showing slider condition end*/
+  }
+	
+  function onQueryError(error) {
+    $(".ulDidYouKnow").append(error.statusText);
+  } 
+
+$(document).on('click','.didyouknw-content .bx-pager-item a',function() {
+	setTimeout(function() {
+		sliderdiyou.startAuto();
+	},1000);
+})
+
+
+});
+
+</script>
