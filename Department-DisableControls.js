22e405fc9a97cdeaee3e00f086bff63c7e64c273
LACOETS <garcia_edward@lacoe.edu>
2016-11-29 13:53:44 -0800

    Add files via upload

--- Department-DisableControls.js
+++ Department-DisableControls.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
 	$("div[id='DataClassificationRSP_$containereditableRegion']").prop('contenteditable',false);
 	$("[title='Browse for a valid choice']").hide();
 	$("textarea[title='Lib Description']").attr('disabled','disabled');
+	$("select[title='Belongs to Project']").prop("disabled", true);
+	$("select[title='Belongs to Project Required Field']").prop("disabled", true);
 });//End of Doc ready
 
 </script>
